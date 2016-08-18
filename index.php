de947a362ec4b9afb8906585077d780a1c9e9c84
fadli <fadli@inotechindo.com>
2016-08-18 15:33:10 +0700

    REDPLOY Test: fixing new line

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
-echo "halo-halo dunia!";
-echo "ini baru kalimat kedua";
-echo "ini kalimat yang ketiga";
-echo "tambah bagian dev";
-echo "tambah lima di dev";
-echo "coba lagi di dev yaaaaaaaaaa";
+echo "halo-halo dunia!<br>";
+echo "ini baru kalimat kedua<br>";
+echo "ini kalimat yang ketiga<br>";
+echo "tambah bagian dev<br>";
+echo "tambah lima di dev<br>";
+echo "coba lagi di dev yaaaaaaaaaa<br>";
+echo "<h3>coba redeploy yeay!</h3><br>";
 
 ?>
