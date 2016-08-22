450be39389374a9f0a5bf6bcc985f971aa5ecff1
fadli <fadli@inotechindo.com>
2016-08-22 21:59:41 +0700

    change on staging deployment using shell script

--- testdeploy.sh
+++ testdeploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-docker login -u fadlihub+otomatis -p VPQ23A35RY5LRFZS63IQ7E0PUTVF4C1SRI7DK5QOCXBP5D0WC3UV8KGS22J1IG5W quay.io;
-docker pull quay.io/fadlihub/hellophp:dev;
-docker run -d -p 8080:80 --name test-hellophp quay.io/fadlihub/hellophp:dev;
+echo "stopping running application"
+ssh $DEPLOY_USER@$DEPLOY_HOST "docker stop test-hellophp"
+
+echo "removing running application"
+ssh $DEPLOY_USER@$DEPLOY_HOST "docker rm test-hellophp"
+
+echo "logging in to registry"
+ssh $DEPLOY_USER@$DEPLOY_HOST "docker login -u $DOCKER_USER -p $DOCKER_PASS -e "$DOCKER_EMAIL" quay.io"
+
+echo "pulling latest version of the code"
+ssh $DEPLOY_USER@$DEPLOY_HOST "docker pull quay.io/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BRANCH"
+
+echo "starting the new version"
+ssh $DEPLOY_USER@$DEPLOY_HOST "docker run -d -p 8080:80 quay.io/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BRANCH"
+
+echo "success!"
+
+exit 0
