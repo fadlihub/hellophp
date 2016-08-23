463cc05c7dbf51096abc0b16c437d2ee5c690799
fadli <fadli@inotechindo.com>
2016-08-23 11:33:43 +0700

    ganti index dan config circle

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-echo "ABCDEF halo-halo dunia!<br>";
-echo "ini baru kalimat kedua<br>";
-echo "ini kalimat yang ketiga<br>";
-echo "tambah bagian dev<br>";
-echo "tambah lima di dev<br>";
-echo "coba lagi di dev yaaaaaaaaaa<br>";
-echo "<h3>Redeploy Test!</h3><br>";
-echo "heheheheheheheheheheheheheheh";
+echo "<h3>test redeploy #1</h3><br>";
 
 ?>
